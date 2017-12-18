e9b60fa3c61031dee96c1b53e1b8d90b7e7562cc
cwowhappy <lixiaoyi1061220211@126.com>
2017-12-18 23:05:23 +0800

    学习

--- study-java-classloader/src/main/java/cwowhappy/study/chapter01/Test.java
+++ study-java-classloader/src/main/java/cwowhappy/study/chapter01/Test.java
@@ -5,10 +5,10 @@ package cwowhappy.study.chapter01;
  */
 public class Test {
     public static void main(String[] args) {
-        ClassLoader classLoader = Test.class.getClassLoader();
-        do {
-            System.out.println(classLoader);
-            classLoader = classLoader.getParent();
-        } while (classLoader != null);
+//        ClassLoader classLoader = Test.class.getClassLoader();
+//        do {
+//            System.out.println(classLoader);
+//            classLoader = classLoader.getParent();
+//        } while (classLoader != null);
     }
 }
---
+++ study-java-concurrency/src/main/java/cwowhappy/study/java/concurrency/StudyCompletableFuture.java
@@ -0,0 +1,47 @@
+package cwowhappy.study.java.concurrency;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class StudyCompletableFuture {
+    private static final Logger LOG = LoggerFactory.getLogger(StudyCompletableFuture.class);
+
+    public static void main(String[] args) {
+        CompletableFuture.supplyAsync(() -> {
+            LOG.info("1");
+            try {
+                Thread.sleep(200L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            LOG.info("1");
+            return 1;
+        }).thenApply((future1) -> CompletableFuture.supplyAsync(() -> {
+            LOG.info("2");
+            try {
+                Thread.sleep(200L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            LOG.info("2");
+            return future1 + 1;
+        })).thenAcceptAsync((result) -> {
+            try {
+                LOG.info("a={}", result.get());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }).join();
+    }
+
+    private void testSync() {
+
+    }
+
+    private void testAsync() {
+
+    }
+}
